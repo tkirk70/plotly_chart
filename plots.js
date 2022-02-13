758b78f5de5449a96235a36c827411a92b473a6f
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-02-13 12:40:57 -0500

    Update plots.js

--- plots.js
+++ plots.js
@@ -14,7 +14,7 @@ var trace1 = {
   x: data.map(row => row.greekSearchResults),
   y: data.map(row => row.greekName),
   text: data.map(row => row.greekName),
-  name: "Greek",
+  name: "Butthole",
   type: "bar",
   orientation: "h"
 };
